f859b37894902235f120e6cbb04f4a1b9e493ca6
nevin0890 <bcab19_2217@santhigiricollege.com>
2022-10-11 10:28:23 +0530

    service 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import{ HttpClientModule } from '@angular/common/http';
 
 const appRoutes: Routes = [
   { path: '', component: ToDoListComponent },
@@ -20,7 +21,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
